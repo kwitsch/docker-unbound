bdcf0797e3d8cf2bcf12128fe12c48fb6c61f2b5
Kwitsch <kwitsch@gmail.com>
2021-03-09 09:24:14 +0100

    failsafes added

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 if [ "$1" = 'unbound' ]; then
     if [ ! -f ./data/root.hints ]; then
         echo "-- Start bootstrap"
+        if [ -f /etc/unbound/unbound.conf ]; then
+            rm /etc/unbound/unbound.conf
+        fi
         ln ./config/bootstrap.conf /etc/unbound/unbound.conf
         unbound
         echo "-----------------"
@@ -12,11 +15,15 @@ if [ "$1" = 'unbound' ]; then
         echo "-----------------"
 
         echo "-- Get root.hints from internic.net"
-        curl -s https://www.internic.net/domain/named.root > ./data/root.hints
+        curl -s https://www.internic.net/domain/named.root > ./data/root.hints || echo "download failed !!"
+        if [ ! -f ./data/root.hints ]; then
+            echo "using bootstrap.hints"
+            ln ./data/bootstrap.hints ./data/root.hints
+        fi
         echo "-----------------"
 
         echo "-- Verify anchor(root.key)"
-        unbound-anchor -4 -v -a ./data/root.key
+        unbound-anchor -4 -v -a ./data/root.key || echo "verification failed !!"
         echo "-----------------"
 
         echo "-- Set permissions"
@@ -32,6 +39,9 @@ if [ "$1" = 'unbound' ]; then
     fi
 
     echo "-- Start unbound"
+    if [ ! -f /etc/unbound/unbound.conf ]; then
+        ln ./config/unbound.conf /etc/unbound/unbound.conf
+    fi
     exec "$@"
 fi
 
